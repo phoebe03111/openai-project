a16ef72ca89be4c6be51b44e8a90da11188f4e30
Phoebe Chang <phoebe03111@gmail.com>
2022-05-23 05:00:11 -0700

    Add link to site logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,9 @@ import "./Header.scss";
 const Header = () => {
   return (
     <header className="header">
-      <h1 className="header__logo">Fun with AI</h1>
+      <a href="/">
+        <h1 className="header__logo">Fun with AI</h1>
+      </a>
     </header>
   );
 };
